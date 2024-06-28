8df25de44ec4256988b8bf01997c788b38a4fca7
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2024-06-28 13:14:31 +0530

    add category, blog model and customise admin panel

---
+++ blogs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+    list_display = ('title', 'category', 'author', 'is_featured', 'created_at', 'status')
+
+    search_fields = ('id', 'category__category_name', 'status')
+
+    list_editable = ('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
