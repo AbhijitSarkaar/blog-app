c945619bb4a826f9b84306f0dcac4ca4047c70f7
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2024-07-01 13:49:36 +0530

    login and logout functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
     path('blogs/<slug:slug>', blogs_views.blogs, name='blogs'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
